d9518906b692e07cbd66996bbb0dd8dde813dbb1
Cihan <adrian.towery@gmail.com>
2017-12-14 21:45:35 +0000

    created page to make new camps and added appropriate callbacks

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+//temp array of cmapgrounds before db is set up
+var camps = [
+    {name: "Salmon Creek", image: "https://farm1.staticflickr.com/732/32796623146_24b830c64c.jpg"},
+    {name: "Granite Hill", image: "https://farm4.staticflickr.com/3347/3445361564_d68d677d58.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://farm9.staticflickr.com/8320/8034434969_1b942bdf2a.jpg"}
+]
 
 app.set("view engine","ejs");
 
@@ -8,15 +17,23 @@ app.get("/", function(req,res){
 });
 
 app.get("/camps", function(req,res){
-    var camps = [
-        {name: "Salmon Creek", image: "https://farm1.staticflickr.com/732/32796623146_24b830c64c.jpg"},
-        {name: "Granite Hill", image: "https://farm4.staticflickr.com/3347/3445361564_d68d677d58.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm9.staticflickr.com/8320/8034434969_1b942bdf2a.jpg"}
-    ]
+
     res.render("camps",{camps:camps});
 });
+//REST convention
+app.post("/camps", function(req,res){
+    //get data and add to array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCamp = {name: name, image: image}
+    camps.push(newCamp);
+    //redirect back to landing
+    res.redirect("/camps");
+});
 
-
+app.get("/camps/new", function(req, res) {
+   res.render("new"); 
+});
 app.listen(process.env.PORT,process.env.IP, function(){
     console.log("baseCamp running");
 });
