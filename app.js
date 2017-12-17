a20a08d4383310c9232133fa381febd21167ffcd
Cihan <adrian.towery@gmail.com>
2017-12-17 18:37:53 +0000

    added data persistence through mongoDB and updated newcamp site

--- app.js
+++ app.js
@@ -1,43 +1,63 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/basecamp");   
 app.use(bodyParser.urlencoded({extended: true}));
-
-//temp array of cmapgrounds before db is set up
-var camps = [
-    {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2918/14003454933_966f43860f.jpg"},
-    {name: "Granite Hill", image: "https://farm3.staticflickr.com/2454/32025488833_b93439d1ec.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm8.staticflickr.com/7238/13984176234_ef0c9649d2.jpg"},
-    {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2918/14003454933_966f43860f.jpg"},
-    {name: "Granite Hill", image: "https://farm3.staticflickr.com/2454/32025488833_b93439d1ec.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm8.staticflickr.com/7238/13984176234_ef0c9649d2.jpg"},
-    {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2918/14003454933_966f43860f.jpg"},
-    {name: "Granite Hill", image: "https://farm3.staticflickr.com/2454/32025488833_b93439d1ec.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm8.staticflickr.com/7238/13984176234_ef0c9649d2.jpg"},
-    {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2918/14003454933_966f43860f.jpg"},
-    {name: "Granite Hill", image: "https://farm3.staticflickr.com/2454/32025488833_b93439d1ec.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://farm8.staticflickr.com/7238/13984176234_ef0c9649d2.jpg"}
-]
-
 app.set("view engine","ejs");
+
+
+//schemas
+var campSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Camp = mongoose.model("Camp", campSchema);
+
+// Camp.create(
+//     {name: "Granite Hill", image: "https://farm3.staticflickr.com/2454/32025488833_b93439d1ec.jpg"},
+//     function(err,camp){
+//         if(err){
+//             console.log(err);
+//         } else{
+//             console.log("NEW CAMP: ");
+//             console.log(camp);
+//         }
+//     });
+
+
+
 
 app.get("/", function(req,res){
     res.render("landing");
 });
 
 app.get("/camps", function(req,res){
-
-    res.render("camps",{camps:camps});
+    //get all camps from db
+    Camp.find({},function(err, allCamps){
+        if(err){
+            console.log(err)
+        } else{
+            res.render("camps",{camps:allCamps});
+        }
+    });
+//    res.render("camps",{camps:camps});
 });
 //REST convention
 app.post("/camps", function(req,res){
     //get data and add to array
-    var name = req.body.name;
-    var image = req.body.image;
-    var newCamp = {name: name, image: image}
-    camps.push(newCamp);
-    //redirect back to landing
-    res.redirect("/camps");
+    var newCamp = req.body.camp;
+    //create new camp and save to db
+    Camp.create(newCamp,function(err, camp){
+        if(err){
+            console.log(err);
+        } else{
+            res.redirect("/camps");
+        }
+    });
+
 });
 
 app.get("/camps/new", function(req, res) {
